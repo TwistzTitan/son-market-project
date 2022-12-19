e591e0cdb9a77e82008256a20ce7e1b70f213e67
Leonardo Honorio <leozinhuhonorio@gmail.com>
2022-12-19 20:48:52 -0300

    feat: added models with migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using market.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace market.Data;
 
 public class ApplicationDbContext : IdentityDbContext
-{
+{   
+    DbSet<Produto> Produtos {get; set;}
+    DbSet<Categoria> Categorias {get; set;}
+    DbSet<Fornecedor> Fornecedores {get; set;}
+    DbSet<Promocao> Promocoes {get; set;}
+    DbSet<Estoque> Estoques {get; set;}
+    DbSet<Saida> Saidas {get; set;}
+    DbSet<Venda> Vendas {get; set;}
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
