51da78fdc1b62e498bc74c9ba243bc340c5b5ae6
Leonardo Honorio <leozinhuhonorio@gmail.com>
2022-12-20 10:58:16 -0300

    build: added categoria status field

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace market.Models
 {
     public class Categoria
     {
         public int Id {get; set;}
         public string Nome {get; set;} = String.Empty;
+        public bool Status {get; set;}
     }
 }
