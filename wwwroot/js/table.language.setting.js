3fd616df8c7ea4f09072dd000b386d5ccc6030c2
Leonardo Honorio <leozinhuhonorio@gmail.com>
2022-12-22 16:42:17 -0300

    feat: added table language settings

---
+++ wwwroot/js/table.language.setting.js
@@ -0,0 +1,14 @@
+
+var SettingsLanguageDefault = {
+    "lengthMenu": "Exibindo _MENU_ registros por página",
+    "zeroRecords": "Nenhum resultado encontrado",
+    "info": "Exibindo _PAGE de _PAGES_",
+    "infoEmpty": "Nenhum resultado disponível",
+    "search": "Buscar: ", 
+    "paginate": {
+        "first": "Primeira",
+        "last" : "Última",
+        "next" : "Próxima",
+        "previous": "Anterior"
+    }
+}
